7ecf7efbd1a5755f6dafc0bca65e8d112b561763
utybe-cpu <utibeudoma2002@.com>
2023-02-23 20:29:02 +0100

    Modified the whole code

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const testimonials = document.querySelectorAll(".testimonial");
-const intervalTime = 5000;
-let index = 0;
-
-function showNextTestimonial() {
-  testimonials[index].classList.remove("active");
-  index = (index + 1) % testimonials.length;
-  testimonials[index].classList.add("active");
-}
-
-setInterval(showNextTestimonial, intervalTime);
+const testimonials = [
+    { text: "I couldn't be happier with the results!", author: "John Doe" },
+    { text: "Amazing service! Highly recommended.", author: "Jane Smith" },
+    { text: "The best decision I've ever made!", author: "Bob Johnson" },
+  ];
+  
+  let currentTestimonial = 0;
+  
+  const testimonialElem = document.getElementById("testimonial");
+  const authorElem = document.getElementById("author");
+  const prevBtn = document.getElementById("prev-btn");
+  const nextBtn = document.getElementById("next-btn");
+  
+  function showTestimonial() {
+    const testimonial = testimonials[currentTestimonial];
+    testimonialElem.innerHTML = `"${testimonial.text}"`;
+    authorElem.innerHTML = `- ${testimonial.author}`;
+  }
+  
+  function showPrev() {
+    currentTestimonial--;
+    if (currentTestimonial < 0) {
+      currentTestimonial = testimonials.length - 1;
+    }
+    showTestimonial();
+  }
+  
+  function showNext() {
+    currentTestimonial++;
+    if (currentTestimonial >= testimonials.length) {
+      currentTestimonial = 0;
+    }
+    showTestimonial();
+  }
+  
+  prevBtn.addEventListener("click", showPrev);
+  nextBtn.addEventListener("click", showNext);
+  
+  showTestimonial();
+  
